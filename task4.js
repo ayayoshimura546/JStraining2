febf3f6cbcae19f29abbec1fecaee2711382a5f8
ayayoshimura546 <133682546+ayayoshimura546@users.noreply.github.com>
2023-06-14 17:32:16 +0900

    second submission

--- task4.js
+++ task4.js
@@ -1,8 +1,8 @@
 // #4 Create an array with duplicate numbers and filter out the duplicates
-var a = [2,4,6,5,4,7,9,8,7,6,13];
 
-var b = a.filter(function (x, i, self) {
-    return self.indexOf(x) === i && i !== self.lastIndexOf(x);
-});
 
-console.log(b);
+const numbers = [2,4,6,5,4,7,9,8,7,6,13];
+
+const uniqueNumbers = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
